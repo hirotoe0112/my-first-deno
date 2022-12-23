e7f8dd44c4608d7d2b6c707d79984d3b3216db1c
hirotoe0112 <hirotoe0112@gmail.com>
2022-12-23 17:41:08 +0900

    readfile

--- http.ts
+++ http.ts
@@ -2,3 +2,6 @@ const url = Deno.args[0]
 const res = await fetch(url)
 const body = new Uint8Array(await res.arrayBuffer())
 await Deno.stdout.write(body)
+
+// コマンド例
+// deno run --allow-net=greenry.jp http.ts https://greenry.jp
