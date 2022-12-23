3666c56e79491caa0dd22da17e886d9e71243254
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2022-12-23 17:43:17 +0900

    readfile

--- readfile.ts
+++ readfile.ts
@@ -11,3 +11,11 @@ filenames.forEach(async (filename) => {
 // deno run --allow-read readfile.ts ./.vscode/settings.json
 
 // 公式サンプルにはasyncなしの例が載っていたがasyncをつけないとエラーになる。なぜ？
+
+// ↑わかった。↓のようにトップレベルのawaitは許されるが、メソッド内で使う場合は従来通りasyncが必要のようだ。
+
+for(const filename of filenames){
+    const file = await Deno.open(filename)
+    await copy(file, Deno.stdout)
+    file.close()
+}
